a28e40bc4514057d34f14270368290649a2afa2e
Captainwills <ohiozuagodswillgmail.com>
2024-06-21 16:43:15 +0100

    .

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,22 +1,17 @@
 var count = 10;
-var gameOver = `<div id="play__again" style = "display: none">
-            <p class="">GAME OVER!</p>
-            <button>Play Again</button>
-        </div>`;
 function getGuess(){
-    // event.preventDefault()
-    var guess = document.getElementById("guess").value;
 
-    // var randomNumber = Math.floor(Math.random(100* 1));
+    var guess = document.getElementById("guess").value;
     const  randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
     var result = document.getElementById("result");
     if (!isNaN(guess)){
-        result.innerHTML = `Your Input is Invalid, Please Input a numer between 1 and 100`;
+        count = count-1
+        result.innerHTML = `Your Input is Invalid, Please Input a numer between 1 and 100, \n you have ${count} left`;
     }
     if (count == 0){
         document.getElementById("input__section").style.display="none";
         document.getElementById("play__again").style.display="";
-        // document.getElementById("play__again").innerHTML= gameOver;
+        
         }
     if (guess == randomNumber){
         result.innerHTML = `Your Guess is Correct!`;
